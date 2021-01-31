9cd378519399b7d681f5ec9b63433e4a626ee41b
Andre Wolden <wolden.andre@gmail.com>
2021-01-31 20:06:16 +0100

    pina colada

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <header>
         <h1>Welcome to Funkoa!</h1>
         <p>Funkoa runs on my private kubernetes raspberry-pi cluster. Here we deploy stuff to have fun and practice full stack coding.</p>
+          <img className={'pina'} src={'/images/pexels-i̇brahim-hakkı-uçman-4134384.jpg'}></img>
       </header>
     </div>
   );
