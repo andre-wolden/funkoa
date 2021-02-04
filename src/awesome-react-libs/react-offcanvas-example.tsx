92ce55105ce814b27408f9fc67e908b4d82d9a92
Andre Wolden <wolden.andre@gmail.com>
2021-02-04 07:41:29 +0100

    fix compile warning

--- src/awesome-react-libs/react-offcanvas-example.tsx
+++ src/awesome-react-libs/react-offcanvas-example.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { OffCanvas, OffCanvasMenu, OffCanvasBody } from "react-offcanvas";
 import MyPlanet from "./react-planet-example";
 
