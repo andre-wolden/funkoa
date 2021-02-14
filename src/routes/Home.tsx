fbc41de17db66456b4723f34cc97f0547aec34a1
Andre Wolden <wolden.andre@gmail.com>
2021-02-14 09:03:48 +0100

    wip styling and stuff

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,17 +5,15 @@ const Home = (): JSX.Element => {
     return (
         <div>
             <h1 className="h1-welcome">Welcome!</h1>
-            <p>
-                FunKoA is a place for KoA to have fun, experiment and practice stuff.
-            </p>
-            <p className="mango-petit">
-                KoA: Karsten og André (which in english will be Karsten and André. Put all this in small cool text,
-                maybe itallic shit.)
-            </p>
-            <p>
-                It runs on our private kubernetes
-                cluster. So far it consists of three Raspberry Pi 4&apos;s.
-            </p>
+            <div className="home-introtext-wrapper">
+                <div className="home-paragraph-1">
+                    FunKoA is a place for KoA to have fun, experiment and practice stuff
+                </div>
+                <p className="home-paragraph-2">
+                    KoA - Karsten og André (which in english will be Karsten and André
+                </p>
+                <p>It runs on our private kubernetes cluster. So far it consists of three Raspberry Pi 4&apos;s.</p>
+            </div>
         </div>
     );
 };
