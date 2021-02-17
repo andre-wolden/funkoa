35235f4556359fefbbc69be85212fb84eca7c3a3
Andre Wolden <wolden.andre@gmail.com>
2021-02-17 15:01:46 +0100

    upgrade logo and header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./header.css";
 
 const Header = (): JSX.Element => {
-    return <div className="sticky-header">I am the Header. Insert logo and the rest.
-        <img alt={"funkoa logo"} src={"/favicon.ico"} />
-
-    </div>;
+    return (
+        <div className="sticky-header">
+            <div className="header-text-center">FunKoA</div>
+            <div className="header-logo-right">
+                <img className="header-koa-logo" alt={"funkoa logo"} src={"/logo.svg"} />
+            </div>
+        </div>
+    );
 };
 
 export default Header;
--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -3,11 +3,11 @@ import "./home.css";
 
 const Home = (): JSX.Element => {
     return (
-        <div>
+        <div className="home-wrapper">
             <h1 className="h1-welcome">Welcome!</h1>
             <div className="home-introtext-wrapper">
                 <div className="home-paragraph-1">FunKoA is a place for KoA to practice stuff, and have fun</div>
-                <p className="home-paragraph-2">KoA - Karsten og André (which in english will be Karsten and André</p>
+                <p className="home-paragraph-2">KoA - Karsten og André (which in english will be Karsten and André)</p>
                 <p>It runs on our private kubernetes cluster. So far it consists of three Raspberry Pi 4&apos;s.</p>
             </div>
         </div>
