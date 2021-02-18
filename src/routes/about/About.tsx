f14b99d553083360b6017a383ea36311131a53b6
Andre Wolden <wolden.andre@gmail.com>
2021-02-18 21:19:37 +0100

    wip on about page. Kubernetes ingress rule update

--- src/routes/about/About.tsx
+++ src/routes/about/About.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import LoremIpsum from "../../components/LoremIpsum";
 import "./about.css";
 import UiPaper from "../../components/paper/UiPaper";
-import StyleHeaderOne, {StyledHeaderSize} from "../../components/stylish-h1/StyleHeaderOne";
+import StyleHeaderOne, { StyledHeaderSize } from "../../components/stylish-h1/StyleHeaderOne";
+import IntroCard from "./IntroCard";
 
 const About = (): JSX.Element => {
     return (
         <div className="about-wrapper">
             <StyleHeaderOne size={StyledHeaderSize.MEDIUM}>ABOUT</StyleHeaderOne>
+            <div className="about-cards-wrapper">
+                <IntroCard />
+                <IntroCard />
+            </div>
             <UiPaper>
                 <LoremIpsum />
             </UiPaper>
