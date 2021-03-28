1ab15635ad6121c4595a06947fb13fb18a83b63f
Andre Wolden <wolden.andre@gmail.com>
2021-03-28 20:31:36 +0200

    random change to test deploy to pi-cluster

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -10,7 +10,7 @@ const Home = (): JSX.Element => {
             <UiPaper>
                 <div className="home-paragraph-1">FunKoA is a place for KoA to practice stuff, and have fun</div>
                 <p className="home-paragraph-2">KoA - Karsten og André (which in english will be Karsten and André)</p>
-                <p>It runs on our private kubernetes cluster. So far it consists of three Raspberry Pi 4&apos;s.</p>
+                <p>It runs on our private kubernetes cluster. So far it consists of three Raspberry Pi 4&apos;s. \0/</p>
             </UiPaper>
         </div>
     );
